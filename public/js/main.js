b7f6d368bedc5c11224035d5c34ae99cefa197e1
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2016-09-09 16:05:01 +0700

    Clean up markup & its interpretation. id:2043

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,15 @@
+function modName(context){
+	return $(context).parent().data('module')
+}
+
 $('button').click(function(){
 	$('#output').text('working...')
 })
 
 $('button.stop').click(function(){
-	$('#output').load('/stop/'+$(this).attr('rel'))
+	$('#output').load('/stop/' + modName(this))
 })
 
 $('button.restart').click(function(){
-	$('#output').load('/restart/'+$(this).attr('rel'))
+	$('#output').load('/restart/' + modName(this))
 })
