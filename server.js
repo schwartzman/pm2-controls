ca209e5cc27d7c68580f75118e3569f127505ba3
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2017-12-15 15:15:30 +0700

    Major changes to enable dynamic discovery and control of whatever pm2 has running. id:2722

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var _ = require('lodash')
-var async = require('async')
-var express = require('express')
-var globalModules = require('global-modules')
-var pm2 = require(globalModules+'/pm2')
-var app = express()
+const _ = require('lodash')
+const async = require('async')
+const express = require('express')
+const globalModules = require('global-modules')
+const nunjucks = require('nunjucks')
+const pm2 = require(globalModules+'/pm2')
+const pm2SelfName = require('./pm2.json').name
+const app = express()
 
 function Directive(action, req){
 	this.action = action
@@ -11,7 +13,7 @@ function Directive(action, req){
 }
 
 function output(err, result, directive, res) {
-	var msg
+	let msg
 	if (err) {
 		console.warn(err)
 		msg = err
@@ -27,14 +29,14 @@ function output(err, result, directive, res) {
 }
 
 function outputList(err, result, res) {
-	var msg
+	let msg
 	if (err) {
 		console.warn(err)
 		msg = err
 	} else {
 		msg = 'Status of all existing processes\n\n'
 		for (var module of result) {
-			msg += _.upperFirst(module.name) + ': ' + module.pm2_env.status + '\n'
+			msg += module.name + ': ' + module.pm2_env.status + '\n'
 		}
 	}
 	res.send(msg)
@@ -42,37 +44,40 @@ function outputList(err, result, res) {
 
 function pm2Action(directive, res){
 	async.waterfall([
-		function(callback){
-			pm2.connect(function(err){
+		(callback) => {
+			pm2.connect((err) => {
 				callback(err)
 			})
 		},
-		function(callback){
-			pm2[directive.action](directive.module, function(err, result){
+		(callback) => {
+			pm2[directive.action](directive.module, (err, result) => {
 				callback(err, result)
 			})
 		}
-	], function(err, result){
+	],
+	(err, result) => {
 		pm2.disconnect()
 		output(err, result, directive, res)
 	})
 }
 
-function pm2List(directive, res){
+function pm2List(inject, callbackTop, res=null){
 	async.waterfall([
-		function(callback){
-			pm2.connect(function(err){
+		(callback) => {
+			pm2.connect((err) => {
 				callback(err)
 			})
 		},
-		function(callback){
-			pm2.list(function(err, result){
+		(callback) => {
+			pm2.list((err, result) => {
 				callback(err, result)
 			})
 		}
-	], function(err, result){
+	],
+	(err, result) => {
 		pm2.disconnect()
-		outputList(err, result, res)
+		const args = inject ? [err, result, res] : [_.without(result.map(x => x.name), pm2SelfName)]
+		callbackTop(...args)
 	})
 }
 
@@ -83,19 +88,32 @@ var loggy = function (req, res, next) {
 
 app.use(loggy, express.static('public'))
 
-app.get('/list', function(req, res){
-	pm2List('list', res)
+app.use(express.static('public'))
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
 })
 
-app.get('/describe/:module', function(req, res){
+app.get('/', (req, res) => {
+	pm2List(false, (procs) => {
+		res.render('index.j2', {procs: procs})
+	})
+})
+
+app.get('/list', (req, res) => {
+	pm2List(true, outputList, res)
+})
+
+app.get('/describe/:module', (req, res) => {
 	pm2Action(new Directive('describe', req), res)
 })
 
-app.get('/stop/:module', function(req, res){
+app.get('/stop/:module', (req, res) => {
 	pm2Action(new Directive('stop', req), res)
 })
 
-app.get('/restart/:module', function(req, res){
+app.get('/restart/:module', (req, res) => {
 	pm2Action(new Directive('restart', req), res)
 })
 
